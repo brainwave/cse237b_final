343c9338daf9089da7eaac3e3334705ae0993d49
brahmprakash.mishra <bpmishra92@gmail.com>
2017-06-15 14:23:11 -0700

    Operator.go inserts into TrackQueue

--- src/main/main.go
+++ src/main/main.go
@@ -1,1 +1,16 @@
+package main
 
+import (
+	"log"
+	"operator"
+)
+
+func main() {
+	log.Printf("Main program starts\n")
+	aspace := operator.NewAirSpace()
+	aspace.Start()
+	log.Printf("Main program ends\n")
+	for {
+
+	}
+}
